0e0ecc561ea49d638cba3fe5ac2d985a58ef04cb
Ashritha-creator <ashrithavelineni3@gmail.com>
2025-05-10 21:53:43 -0400

    Update project.service.ts

--- backend/src/project/project.service.ts
+++ backend/src/project/project.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { Project } from '@prisma/client';
 
 @Injectable()
 export class ProjectService {
